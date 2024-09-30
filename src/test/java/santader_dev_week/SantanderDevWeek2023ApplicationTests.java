50f1e2a6eaec67e10375b3296579fd43c0f47379
Silvio <silvio.sgmtec@gmail.com>
2024-09-30 16:26:17 -0300

    Initial commit

---
+++ src/test/java/santader_dev_week/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package santader_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
